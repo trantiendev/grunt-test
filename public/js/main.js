dba1940ca90b160b5d5de517a065811d04c043e8
Tran Tien <titihx19@gmail.com>
2018-04-17 16:59:48 +0700

    First commit

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@
   'use strict';
   console.log('hello');
 })();
+
+function title_video() {
+  $('section.video-intro > .title-video > h3:gt(0)').hide();
+  setInterval(function() {
+    $('section.video-intro > .title-video > h3:first').fadeOut(1e3).next().fadeIn(1e3).end().appendTo('section.video-intro');
+  }, 3e3);
+}
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,25 @@
   'use strict';
 
   console.log('hello');
+  // title_video();
 })();
+
+function title_video(){
+  // var indexItem = 0;
+  // title_carousel();
+  //
+  // for (var i = 0; i < array.length; i++) {
+  //   array[i]
+  // }
+
+  $("section.video-intro > .title-video > h3:gt(0)").hide();
+
+  setInterval(function() {
+    $('section.video-intro > .title-video > h3:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('section.video-intro');
+  }, 3000);
+};
